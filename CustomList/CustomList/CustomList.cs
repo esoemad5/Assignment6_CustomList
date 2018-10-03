804f7118e84f2fec2bba65d15740965b0f10b44a
esoemad5 <esoemad5@gmail.com>
2018-10-03 10:00:45 -0500

    Wrote method signatures, properties, and fields of CustomListClass. Renammed test methods to match conventions. Asserts now check result vs expected result.

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class CustomList
+    public class CustomList<T>
     {
+        private T[] data;
+        public int Count { get => GetCount(); }
+        private int GetCount()
+        {
+
+        }
+        public T Data { get; set; }
+
+        public void Add(T input)
+        {
+
+        }
+        public void Remove(T input)
+        {
+
+        }
+        public string ToString()
+        {
+
+        }
+        public static CustomList<T> operator+ (CustomList<T> customListA, CustomList<T> customListB)
+        {
+
+        }
+        public static CustomList<T> operator- (CustomList<T> customListA, CustomList<T> customListB)
+        {
+
+        }
+        public static Zip(CustomList<T> customListA, CustomList<T> customListB)
+        {
+
+        }
     }
 }
--- CustomList/CustomListTest/UnitTest1.cs
+++ CustomList/CustomListTest/UnitTest1.cs
@@ -7,42 +7,47 @@ namespace CustomListTest
     [TestClass]
     public class UnitTest1
     {
-        public void TestMethod1()
+        public void Add_ValueToEmptyList_ValueInList()
         {
             //Arrange
             CustomList<int> customList = new CustomList<int>();
-            int value = 16;
+            int expectedResult = 16;
             //Act
-            customList.Add(value);
+            customList.Add(expectedResult);
+            int result = customList[0];
             //Assert
-            Assert.AreEqual(value, customList[0]);
+            Assert.AreEqual(expectedResult, result);
         }
 
         [TestMethod]
-        public void TestMethod2()
-        {
-            //Arrange
-            CustomList<int> customList = new CustomList<int>();
-            int value = 16;
-            int countValue = 1;
-            //Act
-            customList.Add(value);
-            //Assert
-            Assert.AreEqual(countValue, customList.Count);
-        }
-
-        [TestMethod]
-        public void TestMethod3()
+        public void Add_Add2ValuesToEmptyList_2ndValueGoesTo2ndIndex()
         {
             //Arrange
             CustomList<int> customList = new CustomList<int>();
             int value0 = 16;
-            int value1 = 20;
+            int expectedResult = 20;
             //Act
             customList.Add(value0);
-            customeList.Add(value1);
+            customList.Add(expectedResult);
+            int result = customList[1];
             //Assert
-            Assert.AreEqual(value1, customList[1]);
+            Assert.AreEqual(expectedResult, result);
         }
+
+        [TestMethod]
+        public void GetCount_Length1List_1()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int expectedResult = 1;
+            //Act
+            customList.Add(value);
+            int result = customList.Count;
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        
     }
 }
