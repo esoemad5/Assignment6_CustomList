737cb0124b2fb43afe1df9cf595dc1142c2956e2
esoemad5 <esoemad5@gmail.com>
2018-10-03 09:38:04 -0500

    Created list project and test project.

---
+++ CustomList/CustomListTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d9435237-e9a6-4ea5-b15f-340ec44f380d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomList/CustomListTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void TestMethod1()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            //Act
+            customList.Add(value);
+            //Assert
+            Assert.AreEqual(value, customList[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 16;
+            int countValue = 1;
+            //Act
+            customList.Add(value);
+            //Assert
+            Assert.AreEqual(countValue, customList.Count);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value0 = 16;
+            int value1 = 20;
+            //Act
+            customList.Add(value0);
+            customeList.Add(value1);
+            //Assert
+            Assert.AreEqual(value1, customList[1]);
+        }
+    }
+}
